049139ce75ef3ff7a2d108d3a4d3b387f3ecc6c5
Tyler <txchen123@gmail.com>
2017-12-02 15:54:11 -0500

    moved markovify.txt

--- generate/views.py
+++ generate/views.py
@@ -1,4 +1,21 @@
+
 from django.http import HttpResponse
+import markovify
+
+# Get raw text as string.
+with open("markovify.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+
+text.encode("utf-8");
+    
+# Build the model.
+text_model = markovify.Text(text)
+
+str = ""
+
+# Print five randomly-generated sentences
+for i in range(5):
+    str += text_model.make_sentence()
 
 def index(request):
-    return HttpResponse("Hello, world! This is a placeholder for the generated essay.")
+    return HttpResponse(str)
